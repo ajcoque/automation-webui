7a97bc3c2ab73b419ac53ab53fdd95c31cee9417
ajcoque <84734358+ajcoque@users.noreply.github.com>
2021-10-23 18:50:20 -0500

    Se agregaron configuraciones de Log y se actualizo el reporte

--- src/test/java/co/com/sofka/stepdefinition/setup/Setup.java
+++ src/test/java/co/com/sofka/stepdefinition/setup/Setup.java
@@ -6,10 +6,14 @@ import net.serenitybdd.screenplay.actors.OnlineCast;
 import net.thucydides.core.annotations.Managed;
 import org.openqa.selenium.WebDriver;
 
+import org.apache.log4j.PropertyConfigurator;
+import static co.com.sofka.util.Log4jValues.LOG4J_PROPERTIES_FILE_PATH;
+
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 
 public class Setup {
 
+    protected static final String USER_DIR = System.getProperty("user.dir");
     @Managed()
     protected WebDriver browser;
 
@@ -24,9 +28,13 @@ public class Setup {
     }
 
     protected void actorSetupTheBrowser(String actorName) {
+        setUpLog4j2();
         setupBrowser(browser);
         setupUser(actorName, browser);
     }
 
+    protected void setUpLog4j2() {
+        PropertyConfigurator.configure(USER_DIR + LOG4J_PROPERTIES_FILE_PATH.getValue());
+    }
 
 }
